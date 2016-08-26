e6934991f30c6a26865bf7858230f21f9f234e83
Kelvin Wu <kelvinwu1996@gmail.com>
2016-08-26 17:30:57 -0400

    WIP - Continue work on parser.

--- src/Commands/Parser.js
+++ src/Commands/Parser.js
@@ -4,17 +4,61 @@ import CommandParserError from '../Errors/CommandParserError';
  * Parses the signature of a command.
  */
 export default class Parser {
+  /**
+   * Parses a given command signature.
+   * @param {String} signature - The command signature.
+   * @returns {Object} Object containing data on the command signature.
+   */
   static parse(signature) {
     if (!signature.trim()) {
-      throw new CommandParserError('Signature should not be empty.');
+      throw new CommandParserError('Signature cannot not be empty.');
     }
 
-    const signatureRegex = /([^\s]+)(\s*\{\s*(.*?)\s*\})*/;
+    const partition = signature.split(' ');
+    const identifier = partition[0].trim();
 
-    if (!signatureRegex.test(signature)) {
-      throw new CommandParserError(`Signature could not be parsed. Was given => ${signature}.`);
+    if (partition.length > 1) {
+      const matches = partition.slice(1).join(' ').match(/\{\s*(.*?)\s*\}/g);
+      const parameters = Parser.parseParameters(matches);
+
+      return {
+        identifier,
+        parameters,
+      };
     }
 
-    const matches = signatureRegex.exec(signature);
+    return {
+      identifier,
+    };
+  }
+
+  /**
+   * Parses the parameters in a command signature.
+   * @param {Array<String>} parameters - Array of parameters parsed from the signature.
+   * @returns {Array<Object>} Array containing data on the parsed parameters.
+   */
+  static parseParameters(parameters) {
+    return parameters.map(param => Parser.parseArgument(param));
+  }
+
+  /**
+   * Parses a single command argument.
+   * @param {String} argument - The argument.
+   * @returns {Object} Object containing data on the parsed argument.
+   */
+  static parseArgument(argument) {
+    let name = null;
+    let description = null;
+
+    if (argument.indexOf(':') !== -1) {
+      [name, description] = argument.split(':', 2).map(element => element.trim());
+    } else {
+      name = argument;
+    }
+
+    return {
+      name,
+      description,
+    };
   }
 }
