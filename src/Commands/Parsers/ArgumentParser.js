141552a21eee32e28b8b27b735f36fdb5fd7841f
Kelvin Wu <kelvinwu1996@gmail.com>
2016-09-01 17:28:43 -0400

    Start ArgumentParser.

---
+++ src/Commands/Parsers/ArgumentParser.js
@@ -0,0 +1,11 @@
+import stringArgv from 'string-argv';
+import { isNumber, toInteger, toNumber } from 'lodash/lang';
+
+/**
+ * Handles parsing of commands given by users.
+ */
+export default class ArgumentParser {
+  static parse(rules, message) {
+
+  }
+}
