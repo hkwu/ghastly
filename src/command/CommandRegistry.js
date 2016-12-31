3f24db02e28a797029814a5be7c8f639990e7e9b
Kelvin Wu <kelvinwu1996@gmail.com>
2016-12-30 20:17:52 -0500

    Change alias mapping to map to command name instead of command instance.

--- src/command/CommandRegistry.js
+++ src/command/CommandRegistry.js
@@ -1,3 +1,4 @@
+import { isString } from 'lodash/lang';
 import CommandError from '../errors/CommandError';
 import CommandObject from './CommandObject';
 import StringMap from '../util/StringMap';
@@ -19,7 +20,7 @@ export default class CommandRegistry {
 
     /**
      * The command aliases stored in this registry.
-     * @type {StringMap.<CommandObject>}
+     * @type {StringMap.<string>}
      * @private
      */
     this.aliases = new StringMap();
@@ -47,16 +48,21 @@ export default class CommandRegistry {
 
   /**
    * Removes a command from the registry, along with its aliases.
-   * @param {string} name - The command's main trigger.
+   * @param {string} identifier - The command's main trigger or one of its aliases.
    * @returns {CommandObject} The removed `CommandObject`.
    * @throws {CommandError} Thrown if given command name doesn't exist in the registry.
-   * @throws {TypeError} Thrown if the given command name is not a non-empty string.
+   * @throws {TypeError} Thrown if the given command identifier is not a string.
    */
-  unload(name) {
+  unload(identifier) {
+    if (!isString(identifier)) {
+      throw new TypeError('Expected command name to be a string.');
+    }
+
+    const name = this.getMainName(identifier);
     const command = this.commands.get(name);
 
     if (!command) {
-      throw new CommandError(`Attempting to remove a non-existent command: '${name}'.`);
+      throw new CommandError(`Attempting to remove a non-existent command: '${identifier}'.`);
     }
 
     this.unalias(...command.aliases);
@@ -67,14 +73,36 @@ export default class CommandRegistry {
 
   /**
    * Gets a command from the registry.
-   * @param {string} name - The identifier of the command in the registry, or
+   * @param {string} identifier - The identifier of the command in the registry, or
    *   an alias of a command.
    * @returns {?CommandObject} The `CommandObject` in the registry, or `null` if it
    *   couldn't be found.
-   * @throws {TypeError} Thrown if the given command name is not a non-empty string.
+   * @throws {TypeError} Thrown if the given command name is not a string.
    */
-  get(name) {
-    return this.commands.get(name) || this.aliases.get(name) || null;
+  get(identifier) {
+    if (!isString(identifier)) {
+      throw new TypeError('Expected command identifier to be a string.');
+    }
+
+    const name = this.getMainName(identifier);
+
+    return this.commands.get(name) || null;
+  }
+
+  /**
+   * Returns the main name associated with an identifier.
+   * @param {string} identifier - The identifier.
+   * @returns {(string|undefined)} The main name associated with this identifier,
+   *   or `undefined` if none exists.
+   * @throws {TypeError} Thrown if the given command identifier is not a string.
+   * @private
+   */
+  getMainName(identifier) {
+    if (!isString(identifier)) {
+      throw new TypeError('Expected command identifier to be a string.');
+    }
+
+    return this.commands.has(identifier) ? identifier : this.aliases.get(identifier);
   }
 
   /**
@@ -83,12 +111,14 @@ export default class CommandRegistry {
    * @param {CommandObject} command - The command to rename.
    * @returns {CommandRegistry} The instance this method was called on.
    * @throws {CommandError} Thrown if there is a name conflict in the registry.
-   * @throws {TypeError} Thrown if the given command name is not a non-empty string,
+   * @throws {TypeError} Thrown if the given command name is not a string,
    *   or if the given command is not a `CommandObject` instance.
    * @private
    */
   rename(name, command) {
-    if (!(command instanceof CommandObject)) {
+    if (!isString(name)) {
+      throw new TypeError('Expected command name to be a string.');
+    } else if (!(command instanceof CommandObject)) {
       throw new TypeError('Expected command to be an instance of CommandObject.');
     } else if (name === command.name) {
       return this;
@@ -109,19 +139,24 @@ export default class CommandRegistry {
    * @returns {CommandRegistry} The instance this method was called on.
    * @throws {CommandError} Thrown if given command name doesn't exist in the
    *   registry, or if a duplicate alias is detected.
-   * @throws {TypeError} Thrown if the given name and aliases are not non-empty
-   *   strings.
+   * @throws {TypeError} Thrown if the given name and aliases are not strings.
    * @private
    */
   alias(name, ...aliases) {
+    if (!isString(name)) {
+      throw new TypeError('Expected command name to be a string.');
+    }
+
     aliases.forEach((alias) => {
-      if (!this.commands.has(name)) {
+      if (!isString(alias)) {
+        throw new TypeError('Expected command alias to be a string.');
+      } else if (!this.commands.has(name)) {
         throw new CommandError(`Attempting to add alias to non-existent command '${name}'.`);
       } else if (this.aliases.has(alias)) {
         throw new CommandError(`Attempting to add duplicate alias '${alias}' to command '${name}'.`);
       }
 
-      this.aliases.set(alias, this.commands.get(name));
+      this.aliases.set(alias, name);
     });
 
     return this;
@@ -133,13 +168,14 @@ export default class CommandRegistry {
    * @returns {CommandRegistry} The instance this method was called on.
    * @throws {CommandError} Thrown if the given command aliases don't exist in
    *   the registry.
-   * @throws {TypeError} Thrown if the given command aliases are not non-empty
-   *   strings.
+   * @throws {TypeError} Thrown if the given command aliases are not strings.
    * @private
    */
   unalias(...aliases) {
     aliases.forEach((alias) => {
-      if (!this.aliases.has(alias)) {
+      if (!isString(alias)) {
+        throw new TypeError('Expected command alias to be a string.');
+      } else if (!this.aliases.has(alias)) {
         throw new CommandError(`Attempting to remove non-existent alias: '${alias}'.`);
       }
 
