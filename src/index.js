12fab0ad1efeceea9fe5379759f04a061d7d237b
Kelvin Wu <kelvinwu1996@gmail.com>
2017-02-13 17:48:53 -0500

    Update exports.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,10 @@
 import Ghastly from './client/Ghastly';
-import apply from './core/apply';
 import configure from './core/configure';
-import generate from './core/generate';
-import prefix from './middleware/prefix';
 import provide from './middleware/provide';
 
 export {
-  apply,
+  Ghastly,
   configure,
-  generate,
-  prefix,
   provide,
 };
 
