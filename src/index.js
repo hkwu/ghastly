87cbb2614244df08fa4de0e5dd0b68355537cfd3
Kelvin Wu <kelvinwu1996@gmail.com>
2017-02-17 17:07:00 -0500

    Add Dispatcher export. Remove default export.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
+import Dispatcher from './client/Dispatcher';
 import Ghastly from './client/Ghastly';
 import configure from './core/configure';
 import provide from './middleware/provide';
 
 export {
+  Dispatcher,
   Ghastly as Client,
   configure,
   provide,
 };
-
-export default Ghastly;
