fd4ef635e91590e1235216ae64065b727203521a
Kelvin Wu <kelvinwu1996@gmail.com>
2017-02-17 17:06:39 -0500

    Docute.

---
+++ docs/config.js
@@ -0,0 +1,16 @@
+self.$config = {
+  home: 'https://raw.githubusercontent.com/hkwu/ghastly/master/README.md',
+  repo: 'hkwu/ghastly',
+  url: 'https://ghastly.js.org',
+  'edit-link': 'https://github.com/hkwu/ghastly/blob/master/docs',
+  nav: [
+    {
+      title: 'Home',
+      path: '/',
+    },
+    {
+      title: 'Introduction',
+      path: '/introduction',
+    },
+  ],
+};
