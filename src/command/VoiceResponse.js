21825a7f8447afeb398bef8b232c0fba31a83867
Kelvin Wu <kelvinwu1996@gmail.com>
2017-02-20 21:52:15 -0500

    VoiceResponse.

---
+++ src/command/VoiceResponse.js
@@ -0,0 +1,39 @@
+import Response from './Response';
+
+/**
+ * @classdesc Wrapper for voice channel audio responses.
+ */
+export default class VoiceResponse extends Response {
+  /**
+   * Constructor.
+   * @param {string} streamType - The type of stream to use.
+   * @param {(ReadableStream|string)} stream - The stream to play, or a string
+   *   containing the path to a file.
+   * @param {StreamOptions} [options={}] - The options for playing the stream.
+   *   See [StreamOptions]{@link https://discord.js.org/#/docs/main/master/typedef/StreamOptions}.
+   */
+  constructor(streamType, stream, options = {}) {
+    super(({ message }) => {
+      if (!message.guild) {
+        throw new Error('Cannot send audio replies outside of a guild.');
+      }
+
+      const connection = message.guild.voiceConnection;
+
+      if (!connection) {
+        throw new Error('Cannot send audio replies without a voice connection.');
+      }
+
+      switch (streamType) {
+        case 'convertedStream':
+          return connection.playConvertedStream(stream, options);
+        case 'file':
+          return connection.playFile(stream, options);
+        case 'stream':
+          return connection.playStream(stream, options);
+        default:
+          throw new Error(`Invalid stream type: ${streamType}.`);
+      }
+    });
+  }
+}
