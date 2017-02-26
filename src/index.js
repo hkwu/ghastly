5d96ab504f0c1fa99709ed54ae0d118d8923f7cb
Kelvin Wu <kelvinwu1996@gmail.com>
2017-02-26 15:05:03 -0500

    Export response classes.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
+import CodeResponse from './command/CodeResponse';
 import Dispatcher from './client/Dispatcher';
 import Ghastly from './client/Ghastly';
+import Response from './command/Response';
+import VoiceResponse from './command/VoiceResponse';
 import configure from './core/configure';
 import provide from './middleware/provide';
 
 export {
+  CodeResponse,
   Dispatcher,
   Ghastly as Client,
+  Response,
+  VoiceResponse,
   configure,
   provide,
 };
