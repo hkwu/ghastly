fe667a7323031029e92fe21f009c48f08c115059
Kelvin Wu <kelvinwu1996@gmail.com>
2017-03-28 20:22:30 -0400

    Change exports for custom responses.

---
+++ src/command/index.js
@@ -0,0 +1,9 @@
+import CodeResponse from './responses/CodeResponse';
+import Response from './responses/Response';
+import VoiceResponse from './responses/VoiceResponse';
+
+export {
+  CodeResponse,
+  Response,
+  VoiceResponse,
+};
--- src/index.js
+++ src/index.js
@@ -1,15 +1,9 @@
-import CodeResponse from './command/responses/CodeResponse';
 import Ghastly from './client/Ghastly';
-import Response from './command/responses/Response';
-import VoiceResponse from './command/responses/VoiceResponse';
 import configure from './core/configure';
 import provide from './middleware/provide';
 
 export {
-  CodeResponse,
   Ghastly as Client,
-  Response,
-  VoiceResponse,
   configure,
   provide,
 };
