7c86681fe882547d7bbd7f8a1d0765bc8891c5c0
Kelvin Wu <kelvinwu1996@gmail.com>
2017-03-29 21:08:25 -0400

    expectRole() middleware.

---
+++ src/middleware/expectRole.js
@@ -0,0 +1,35 @@
+import { isString } from 'lodash/lang';
+
+/**
+ * Middleware which filters out messages based on user role IDs or names. Only
+ *   filters out messages if in a guild context.
+ * @param {...string} identifiers - The role IDs or names which are allowed to
+ *   pass the filter.
+ * @return {middlewareLayer} The layer which filters the messages.
+ * @throws {TypeError} Thrown if the identifiers aren't strings.
+ */
+export default function expectRole(...identifiers) {
+  identifiers.forEach((id) => {
+    if (!isString(id)) {
+      throw new TypeError('Expected role identifiers to be strings.');
+    }
+  });
+
+  const whitelist = new Set(identifiers);
+
+  return async (next, context) => {
+    const { member } = context;
+
+    if (!member) {
+      return next(context);
+    }
+
+    for (const [id, role] of member.roles) {
+      if (whitelist.has(id) || whitelist.has(role.name)) {
+        return next(context);
+      }
+    }
+
+    return false;
+  };
+}
--- src/middleware/index.js
+++ src/middleware/index.js
@@ -1,7 +1,9 @@
+import expectRole from './expectRole';
 import expectUser from './expectUser';
 import provide from './provide';
 
 export {
+  expectRole,
   expectUser,
   provide,
 };
