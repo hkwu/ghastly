8dd1aecd94a2ea0c760633fc20f400f8e2dc980a
Kelvin Wu <kelvinwu1996@gmail.com>
2017-03-29 21:37:38 -0400

    Permissions based filtering.

---
+++ src/middleware/expectPermissions.js
@@ -0,0 +1,32 @@
+import { isNumber, isString } from 'lodash/lang';
+
+/**
+ * Middleware which filters out messages based on user permissions. Members must
+ *   possess all of the given permissions to pass the filter. Only filters out
+ *   messages if in a guild context.
+ * @param {...PermissionResolvable} permissions - The permissions required to
+ *   pass the filter. See [PermissionResolvable]{@link https://discord.js.org/#/docs/main/stable/typedef/PermissionResolvable}.
+ * @return {middlewareLayer} The layer which filters the messages.
+ * @throws {TypeError} Thrown if the permissions aren't the right type.
+ */
+export default function expectPermissions(...permissions) {
+  permissions.forEach((id) => {
+    if (!(isNumber(id) || isString(id))) {
+      throw new TypeError('Expected role identifiers to be strings.');
+    }
+  });
+
+  return async (next, context) => {
+    const { member } = context;
+
+    if (!member) {
+      return next(context);
+    }
+
+    const shouldFilter = permissions.reduce((should, permission) => (
+      should || !member.hasPermission(permission)
+    ), false);
+
+    return !shouldFilter && next(context);
+  };
+}
--- src/middleware/index.js
+++ src/middleware/index.js
@@ -1,8 +1,10 @@
+import expectPermissions from './expectPermissions';
 import expectRole from './expectRole';
 import expectUser from './expectUser';
 import provide from './provide';
 
 export {
+  expectPermissions,
   expectRole,
   expectUser,
   provide,
