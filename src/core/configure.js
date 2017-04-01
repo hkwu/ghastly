b45b230800d7077494f45ab7ee380f746ee4b45a
Kelvin Wu <kelvinwu1996@gmail.com>
2017-04-01 10:55:32 -0400

    Shouldn't have a default.

--- src/core/configure.js
+++ src/core/configure.js
@@ -7,15 +7,14 @@ import { isFunction, isPlainObject } from 'lodash/lang';
  *   generator with the given configuration applied to it.
  * Several configure() calls can be made successively on the returned generator,
  *   with the outer config objects overwriting the keys in the innermost configs.
- * @param {(Object|Function)} [configurator={}] - An object containing the
- *   configuration data to pass to the command generator, or a function which
- *   consumes the configuration produced by the generator and returns a new
- *   configuration.
+ * @param {(Object|Function)} configurator - An object containing the configuration
+ *   data to pass to the command generator, or a function which consumes the
+ *   configuration produced by the generator and returns a new configuration.
  * @returns {Function} A function consuming a generator and producing another plugin
  *   with the provided config applied to it.
  * @throws {TypeError} Thrown if the given config argument is not a plain object.
  */
-export default function configure(configurator = {}) {
+export default function configure(configurator) {
   if (!(isPlainObject(configurator) || isFunction(configurator))) {
     throw new TypeError('Expected given config to be a plain object or function.');
   }
