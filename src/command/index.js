f69069f8832337ac5b2ef16dd437a81cad386d6f
Kelvin Wu <kelvinwu1996@gmail.com>
2017-10-22 20:14:20 -0700

    Add exports.

--- src/command/index.js
+++ src/command/index.js
@@ -1,7 +1,19 @@
+import ArbitraryVoiceResponse from './responses/ArbitraryVoiceResponse';
+import BroadcastVoiceResponse from './responses/BroadcastVoiceResponse';
+import ConvertedVoiceResponse from './responses/ConvertedVoiceResponse';
+import FileVoiceResponse from './responses/FileVoiceResponse';
+import OpusVoiceResponse from './responses/OpusVoiceResponse';
+import StreamVoiceResponse from './responses/StreamVoiceResponse';
 import Response from './responses/Response';
 import VoiceResponse from './responses/VoiceResponse';
 
 export {
+  ArbitraryVoiceResponse,
+  BroadcastVoiceResponse,
+  ConvertedVoiceResponse,
+  FileVoiceResponse,
+  OpusVoiceResponse,
+  StreamVoiceResponse,
   Response,
   VoiceResponse,
 };
